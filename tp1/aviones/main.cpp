a9ed0f02c39ae71e15ca1d08987c202b1a34ea36
Ignacio Garay <imgarayojeda@gmail.com>
2013-10-15 09:45:31 -0300

    agregado un generador tentativo

--- tp1/aviones/main.cpp
+++ tp1/aviones/main.cpp
@@ -1,9 +1,24 @@
 #ifndef GENERADOR_AVIONES_H
 #define GENERADOR_AVIONES_H
 
-#include "Fifo.h"
+#include "FifoEscritura.h"
+#include "Avion.h"
+#include "FactoryElementos.h"
 
 int main(int argc, char** argv) {
+	FifoEscritura fifo("/tmp/fifo_generador");
+	fifo.abrir();
+	// probamos con 20 aviones al principio
+	int i = 20;
+	while(i) {
+		Avion* avioneta = FactoryElementos::instance().crearAvion(i);
+		std::string serializacion = avioneta->serializar();
+		fifo.escribir((const void*)serializacion.c_str(), (ssize_t) serializacion.size());
+		delete avioneta;
+	}
+	fifo.cerrar();
+	fifo.eliminar();
+	return 0;
 }
 
 #endif
