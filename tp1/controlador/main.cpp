dd7316b890db3347f7f1586c4e0e76fee0ecc100
Ignacio Garay <imgarayojeda@gmail.com>
2013-10-16 01:14:50 -0300

    lalal

--- tp1/controlador/main.cpp
+++ tp1/controlador/main.cpp
@@ -1,3 +1,20 @@
+
+
 int main(int argc, char** argv) {
+	FifoLectura fifo(argv[1]);
+	int resultado = 1;
+	do {
+		char* ptr = new char[33];
+		ptr[32] = '\0';
+		resultado = fifo.leer(ptr, 32);
+		if (resultado != 0) {
+			std::string serial = ptr;
+			Avion* avioneta = new Avion(serial);
+			avioneta->operar();
+			delete avioneta;
+		}
+		delete ptr;
+	} while(resultado != 0);
+	
 	return 0;
 }
