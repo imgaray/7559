e89d28f8933766df7ecb88ef85b6342811a01d85
mat1204 <mat1204.torres@gmail.com>
2013-11-23 22:37:21 -0300

    Subida de mains

---
+++ tp2/src/logger/Logger.cpp
@@ -0,0 +1,134 @@
+#include "Logger.h"
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+Logger* Logger::logger_instance = NULL;
+
+Logger::Logger(): nivelDeLog(LOG_DEBUG) {
+	string timeStamp = "output.html";
+	struct stat buffer;
+	if (stat(timeStamp.c_str(), &buffer)) {
+		archivoLog.open( timeStamp.c_str() );
+		printHeader();
+	} else {
+		archivoLog.open(timeStamp.c_str(), std::ofstream::app);
+	}
+	lock = new LockFile(timeStamp.c_str());
+}
+
+Logger& Logger::instance() {
+	if (!logger_instance) {
+		logger_instance = new Logger();
+	}
+	return *logger_instance;
+}
+
+void Logger::close() {
+	if (logger_instance)
+		delete logger_instance;
+	logger_instance = NULL;
+}
+
+Logger::~Logger() {
+	if (archivoLog.is_open()) {
+		archivoLog << "</html>";
+		archivoLog.close();
+	}
+	delete lock;
+}
+
+void Logger::warn(const string& tag, const string& msg){
+	if ((nivelDeLog & LOG_WARN) == 0)
+		return;
+
+	log(tag, msg, LOG_WARN);
+}
+
+void Logger::error(const string& tag, const string& msg) {
+	if ((nivelDeLog & LOG_ERROR) == 0)
+		return;
+
+	log(tag, msg, LOG_ERROR);
+}
+
+void Logger::debug(const string& tag, const string& msg) {
+	if (nivelDeLog != LOG_DEBUG)
+		return;
+	log(tag, msg, LOG_DEBUG);
+}
+
+void Logger::info(const string& tag, const string& msg) {
+	if ((nivelDeLog & LOG_INFO) == 0)
+		return;	
+	
+	log(tag, msg, LOG_INFO);
+}
+
+
+void Logger::fatal(const string& tag, const string& msg) {
+	if ((nivelDeLog & LOG_FATAL) == 0)
+		return;
+	log(tag, msg, LOG_FATAL);
+}
+
+void Logger::log(const string& tag, const string& msg, int level) {
+	lock->tomarLock();
+	string sNivel("[INFO]\t");
+	switch (level) {
+	case LOG_FATAL:
+		sNivel = "[FATAL]\t";
+		break;
+	case LOG_ERROR:
+		sNivel = "[ERROR]\t";
+		break;
+	case LOG_WARN:
+		sNivel = "[WARN]\t";
+		break;
+	case LOG_INFO:
+		sNivel = "[INFO]\t";
+		break;
+	case LOG_DEBUG:
+		sNivel = "[DEBUG]\t";
+		break;
+	default:
+		break;
+	}
+	printMessageFormatted(sNivel, tag, msg);
+	archivoLog.flush();
+	lock->liberarLock();
+}
+
+void Logger::clear() {
+	lock->tomarLock();
+	string timeStamp = "output.html";
+	archivoLog.close();
+	archivoLog.open(timeStamp.c_str());
+	printHeader();
+	lock->liberarLock();
+}
+
+void Logger::setLogLevel(int nivelLog){
+	lock->tomarLock();
+	nivelDeLog = nivelLog;
+	lock->liberarLock();
+}
+
+void Logger::printHeader() {
+	archivoLog << "<html>" << std::endl;
+	archivoLog << "<title>Log de simulacion de aeropuerto</title>" << std::endl;
+	archivoLog << "<header align=\"center\"><strong> Log de simulacion de aeropuerto </strong></header>" << std::endl;
+	archivoLog << "<table id=\"table1\" class=\"mytable\" cellspacing=\"2\" cellpadding=\"10\" >" << std::endl;
+	archivoLog << "<tr><th>Tipo</th><th>Lugar del mensaje</th><th align=\"left\">Mensaje</th></tr>" << std::endl;
+}
+
+void Logger::printMessageFormatted(const std::string& level, const std::string& tag,
+										const std::string& message) {
+	archivoLog << "<tr>" << std::endl;
+	archivoLog << "<td>" << level << "</td>"<<std::endl;
+	archivoLog << "<td>" << tag << "</td>" <<std::endl;
+	archivoLog << "<td>" << message << "</td>" <<std::endl;
+	archivoLog << "</tr>" << std::endl;
+}
---
+++ tp2/src/logger/Logger.h
@@ -0,0 +1,54 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <time.h>
+#include "LockFile.h"
+
+using namespace std;
+
+#define NIVEL_GLOBAL LOG_DEBUG
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL
+
+#define LOG_INFO 1
+#define LOG_WARN 2
+#define LOG_ERROR 4
+#define LOG_FATAL 8
+#define LOG_DEBUG 15
+
+#endif /*LOG_LEVEL*/
+
+class Logger {
+
+private:
+	Logger();
+	~Logger();
+	ofstream archivoLog;
+	// nivel de log con el que logeamos. Por defecto, DEBUG.
+	int nivelDeLog;
+	void log(const string& tag, const string& msg, int level);
+	LockFile* lock;
+	static Logger* logger_instance;
+	void printHeader();
+	void printMessageFormatted(const std::string& level, const std::string& tag,
+				const std::string& message);
+	
+public:
+	void fatal(const string&, const string&);
+	void error(const string&, const string&);
+	void warn(const string&, const string&);
+	void debug(const string&, const string&);
+	void info(const string&, const string&);
+	void setLogLevel(int);
+	void clear();
+	static Logger& instance();
+	static void close();
+};
+
+#endif /* LOGGER_H_ */
